1791e0a67fc90970c41d5a758a76146434ac4798
Felipe Sakuma <felipe.sakuma@exati.com.br>
2024-09-06 14:14:03 -0300

    81. Update useCurrencify Composable

--- src/use/useCurrencify.js
+++ src/use/useCurrencify.js
@@ -1,8 +1,11 @@
+import { useStoreSettings } from "src/stores/storeSettings";
+
+const storeSettings = useStoreSettings();
+
 export function useCurrencify(amount) {
-  // format: "+ $ 4,999.99" | "- $ 999.00"
   const sign = amount > 0 ? "+" : amount < 0 ? "-" : "";
 
-  const currencySymbol = "$";
+  const currencySymbol = storeSettings.settings.currencySymbol;
 
   const amountFormatted = Math.abs(amount).toLocaleString("en-US", {
     minimumFractionDigits: 2,
