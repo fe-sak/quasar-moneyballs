e6917dfbe43c0fa176fcad01b7978b770b860540
Felipe Sakuma <felipe.sakuma@exati.com.br>
2024-09-13 12:31:39 -0300

    84. Dark Plugin

--- src/stores/storeSettings.js
+++ src/stores/storeSettings.js
@@ -1,13 +1,21 @@
 import { defineStore } from "pinia";
-import { reactive } from "vue";
+import { reactive, watch } from "vue";
+import { Dark } from "quasar";
 
 export const useStoreSettings = defineStore("settings", () => {
   const settings = reactive({
     promptToDelete: true,
     showRunningBalance: false,
     currencySymbol: "$",
-    darkMode: false, // false | true | 'auto' 
+    darkMode: false, // false | true | 'auto'
   });
+
+  watch(
+    () => settings.darkMode,
+    (value) => {
+      Dark.set(value);
+    }
+  );
 
   // state
 
